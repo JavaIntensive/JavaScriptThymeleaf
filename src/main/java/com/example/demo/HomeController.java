0beb96949042799f84029b99ec9e69fea0c998e5
rightbil <rightbil@gmail.com>
2020-03-08 15:54:01 -0400

    Date field added it works by adding in the constructor

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -9,6 +11,8 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("demo")
 public class HomeController {
+@Autowired
+TeamRepository teamRepository;
 
     @RequestMapping("mypage")
     public String showPage(Model model){
@@ -21,21 +25,34 @@ public class HomeController {
         p1.setProjectrole("Manager");
 
         list.add(p1);
-
+        teamRepository.save(p1);
         Team p2= new Team();
         p2.setName("Eric");
         p2.setAge(27);
         p2.setProjectrole("Developer");
 
         list.add(p2);
+        teamRepository.save(p2);
 
         Team p3= new Team();
         p3.setName("Bili");
         p3.setAge(27);
         p3.setProjectrole("Tester");
         list.add(p3);
-
+        teamRepository.save(p3);
         model.addAttribute("myteam", list);
         return "mypage";
+    }
+
+    @GetMapping("/showTeam")
+    public String showTeam(Model model){
+
+        Team p1 = new Team();
+        p1.setName("Matt");
+        p1.setAge(27);
+        p1.setProjectrole("Manager");
+        teamRepository.save(p1);
+        model.addAttribute("showTeam", teamRepository.findAll());
+        return "showTeam";
     }
 }
--- src/main/java/com/example/demo/Team.java
+++ src/main/java/com/example/demo/Team.java
@@ -1,31 +1,54 @@
 package com.example.demo;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
 public class Team {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+   /* private LocalDate date;*/
+    private LocalDateTime dateTime;
     private String name;
     private int age;
     private String projectrole;
+    public Team(){
+       /* date= DateFormat(LocalDate.now());*/
+        dateTime= LocalDateTime.now();
+    }
     public String getName() {
         return name;
     }
-
     public String getProjectrole() {
         return projectrole;
     }
-
     public void setProjectrole(String projectrole) {
         this.projectrole = projectrole;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
+  /*  public LocalDate getDate() {
+        return date;
+    }*/
+/*
+    public void setDate() {
+        LocalDate date= LocalDate.now();
+        this.date = date;
+    }*/
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+/*
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }*/
 }
